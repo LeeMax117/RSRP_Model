33068197273823d24bf6c9066807b173de013210
nijilong1224 <42104427+nijilong1224@users.noreply.github.com>
2019-06-14 03:49:17 -0500

    Add files via upload
    New script can drop data based on threshold.

--- songhai_demo/dropRSRP.py
+++ songhai_demo/dropRSRP.py
@@ -1,12 +1,16 @@
 import pandas as pd
 
 df = pd.read_csv('train.csv')
+threshold = 3
 
 
 def mapper(label):
+    cnt = 0
     row = df.iloc[label, 2:]
     for item in row:
         if item > -126.0:
+            cnt += 1
+        if item > -100.0 or cnt >= threshold:
             return True
     return False
 
@@ -14,4 +18,4 @@ def mapper(label):
 new_df = df.select(mapper)
 print(df.describe())
 print(new_df.describe())
-new_df.to_csv('train_new.csv', sep=',', header=True, index=False)
+new_df.to_csv('train_new_2.csv', sep=',', header=True, index=False)
