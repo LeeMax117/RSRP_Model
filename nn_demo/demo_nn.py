a737da546c2d187c53345b0587194d07659823d5
LeeMax117 <43522414+LeeMax117@users.noreply.github.com>
2019-07-16 17:34:50 +0800

    Add files via upload

--- nn_demo/demo_nn.py
+++ nn_demo/demo_nn.py
@@ -2,97 +2,73 @@ import torch
 from torch.autograd import Variable
 import torch.nn
 import torch.optim as optim
-import torch.nn.functional as F
-import numpy as np
+from pylab import *
 import pandas as pd # data processing, CSV file I/O
-from dataprocess import DataSet
+from utils import *
 
 # input the dataset
 # path to where the data lies
 #dpath = './data/'
-full_data = pd.read_csv("D:\Work\AI_competation\dataset\\train_new.csv")
+# full_data = pd.read_csv("D:\Work\AI_competation\dataset\\train.csv")
+full_data = pd.read_csv("D:\Work\AI_competation\yuyang_Demo\\train_new.csv")
 train_data = DataSet(full_data)
 
-class Net(torch.nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
+def val_in_test_set(epoch_num):
+    # load test set and predict
+    test_full = pd.read_csv("D:\Work\AI_competation\dataset\\sample_submission.csv")
+    test = DataSet(test_full)
 
-        '''
-        self.conv1 = torch.nn.Conv2d(2, 20, (1, 1))
-        torch.nn.init.normal_(self.conv1.weight, mean=0, std=1)
-        self.BN1 = torch.nn.BatchNorm2d(20, 1, 6)
-        self.conv2 = torch.nn.Conv2d(20, 200, (1, 6))
-        torch.nn.init.normal_(self.conv2.weight, mean=0, std=1)
-        self.BN2 = torch.nn.BatchNorm2d(200, 1, 1)
-        self.fc1 = torch.nn.Linear(200, 30)
-        torch.nn.init.normal_(self.fc1.weight, mean=0, std=1)
-        self.fc2 = torch.nn.Linear(30, 2)
-        torch.nn.init.normal_(self.fc2.weight, mean=0, std=1)
-        '''
-        # for the first 1*1 and 1*6 conv
-        self.conv1 = torch.nn.Conv2d(2, 20, (1, 1))
-        self.BN1 = torch.nn.BatchNorm2d(20, 1, 6)
-        self.conv2 = torch.nn.Conv2d(20, 200, (1, 6))
+    while test.epochs_completed == 0:
+        test_data, test_label = test.next_batch(100, is_training=False)
+        inputs = Variable(torch.Tensor(test_data))
+        outputs = net(inputs)
+        ground_truth = Variable(torch.Tensor(test_label))
 
-        # the bypass of 1*6 and 1*1 conv
-        self.conv_1 = torch.nn.Conv2d(2,20,(1,6))
-        self.BN_1 = torch.nn.BatchNorm2d(20,1,2)
-        self.conv_2 = torch.nn.Conv2d(20, 200, (1,1))
+        try:
+            predict = torch.cat((predict, outputs), 0)
+            labels = torch.cat((labels, ground_truth), 0)
+        except NameError:
+            predict = outputs
+            labels = ground_truth
 
-        # full connected
-        self.BN2 = torch.nn.BatchNorm2d(400, 1, 1)
-        self.fc1 = torch.nn.Linear(400, 30)
-        self.fc2 = torch.nn.Linear(30, 2)
+    MSE_score = criterion(predict, labels)
+    np_outputs = outputs.detach().numpy()
+    print('last outputs:(%.3f,%.3f), last ground truth:(%.3f,%.3f)' % \
+          (np_outputs[-1][0], np_outputs[-1][1], test_label[-1][0], test_label[-1][1]))
+    print('MSE_score:', MSE_score)
 
-        torch.nn.init.normal_(self.conv1.weight, mean=0, std=1)
-        torch.nn.init.normal_(self.conv2.weight, mean=0, std=1)
-        torch.nn.init.normal_(self.conv_1.weight, mean=0, std=1)
-        torch.nn.init.normal_(self.conv_2.weight, mean=0, std=1)
-        torch.nn.init.normal_(self.fc1.weight, mean=0, std=1)
-        torch.nn.init.normal_(self.fc2.weight, mean=0, std=1)
+    predict = predict.detach().numpy()
+    test_label = labels.detach().numpy()
+    # plot the result
+    x_label = test_label[:, 0]
+    y_label = test_label[:, 1]
 
-    def forward(self, x):
-        # leaky_r = torch.nn.LeakyReLU(6e-10)
-        # use 1*1 to specify which frequency and which position
-        x_1 = self.conv1(x)
-        x_1 = torch.tanh(self.BN1(x_1))
-        # use 1*6*200 to specify 3 position (x,y) and 3 signal and 1 posibility, 20 choice make 200
-        # x = F.relu(self.conv2(x))
-        x_1 = self.conv2(x_1)
-        x_2 = self.conv_1(x)
-        x_2 = torch.tanh((self.BN_1(x_2)))
-        x_2 = self.conv_2(x_2)
-        x = torch.cat((x_1,x_2),1)
-        x = torch.tanh(self.BN2(x))
-        x = x.view(-1, self.num_flat_features(x))
-        # x = F.relu(self.fc1(x))
-        x = F.relu(self.fc1(x))
-        # x = F.relu(self.fc2(x))
-        x = self.fc2(x)
-        return x
+    x_pre = predict[:, 0]
+    y_pre = predict[:, 1]
 
-    def num_flat_features(self, x):
-        size = x.size()[1:] # all dimensions except the batch dimension
-        num_features = 1
-        for s in size:
-            num_features *= s
-        return num_features
+    plt.scatter(x_label, y_label)
+    plt.scatter(x_pre, y_pre)
+    xlabel('x')
+    ylabel('y')
+    title('%d epochs'%(epoch_num))
+    text(150, 80,'MSE_score:%.3f'%(MSE_score),fontsize=15)
+    plt.show()
+
+    return MSE_score
 
 net = Net()
 # define loss
 
 criterion = torch.nn.MSELoss()
 lr = 0.01
-batch_size = 500
+batch_size = 50
 # create your optimizer
-for epoch in range(10):  # loop over the dataset multiple times
+for epoch in range(30):  # loop over the dataset multiple times
 
     optimizer = optim.Adam(net.parameters(), lr=lr)
-    lr = lr * 0.9
-
     current_epoch = train_data.epochs_completed
     steps = 0
-    print('epoch completed:%d'%(current_epoch))
+    print('epoch completed:%d , learning_rate: %.3f'%(current_epoch,lr))
 
     while current_epoch == train_data.epochs_completed:
         # wrap them in Variable
@@ -110,8 +86,8 @@ for epoch in range(10):  # loop over the dataset multiple times
         for param in net.parameters():
             l1_loss += torch.sum(abs(param))
             l2_loss += torch.sum(param ** 2)
-        # total_loss = loss + (3e-5) * l2_loss
-        total_loss = loss
+        total_loss = loss + (7e-2) * l2_loss
+        # total_loss = loss
         total_loss.backward()
         optimizer.step()
         steps += 1
@@ -129,4 +105,18 @@ for epoch in range(10):  # loop over the dataset multiple times
           (np_outputs[-1][0], np_outputs[-1][1], labels[-1][0], labels[-1][1]))
     print('####################################')
     batch_size *= 1.0
+    lr = lr * 0.9
+    test_score = val_in_test_set(current_epoch)
+    try:
+        if test_score < best_score:
+            best_score = test_score
+            torch.save(net.state_dict(), 'D:\Work\AI_competation\yuyang_Demo\ckpt\\best_model')
+            print('**********model saved*************')
+    except NameError:
+        best_score = test_score
+        torch.save(net.state_dict(), 'D:\Work\AI_competation\yuyang_Demo\ckpt\\best_model')
+
 print('Finished Training')
+
+# save the model
+torch.save(net.state_dict(),'D:\Work\AI_competation\yuyang_Demo\ckpt\\final_model')
---
+++ nn_demo/inference.py
@@ -0,0 +1,50 @@
+import torch
+import pandas as pd
+from utils import *
+from torch.autograd import Variable
+from pylab import *
+
+net = Net()
+ckpt_path = 'D:\Work\AI_competation\yuyang_Demo\ckpt\\best_model'
+net.load_state_dict(torch.load(ckpt_path))
+criterion = torch.nn.MSELoss()
+
+# load test set and predict
+test_full = pd.read_csv("D:\Work\AI_competation\dataset\\sample_submission.csv")
+test = DataSet(test_full)
+
+while test.epochs_completed == 0:
+    test_data, test_label = test.next_batch(100, is_training=False)
+    inputs = Variable(torch.Tensor(test_data))
+    outputs = net(inputs)
+    ground_truth = Variable(torch.Tensor(test_label))
+
+    try:
+        predict = torch.cat((predict, outputs), 0)
+        labels = torch.cat((labels, ground_truth), 0)
+    except NameError:
+        predict = outputs
+        labels = ground_truth
+
+MSE_score = criterion(predict, labels)
+np_outputs = outputs.detach().numpy()
+print('last outputs:(%.3f,%.3f), last ground truth:(%.3f,%.3f)' % \
+      (np_outputs[-1][0], np_outputs[-1][1], test_label[-1][0], test_label[-1][1]))
+print('MSE_score:', MSE_score)
+
+predict = predict.detach().numpy()
+test_label = labels.detach().numpy()
+# plot the result
+x_label = test_label[:, 0]
+y_label = test_label[:, 1]
+
+x_pre = predict[:, 0]
+y_pre = predict[:, 1]
+
+plt.scatter(x_label, y_label)
+plt.scatter(x_pre, y_pre)
+xlabel('x')
+ylabel('y')
+title('test_set')
+text(150, 80,'MSE_score:%.3f'%(MSE_score),fontsize=15)
+plt.show()
